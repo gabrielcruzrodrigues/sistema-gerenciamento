9b9c0a596870908bf7dad17798ffefac0292a9f9
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-13 19:52:39 -0300

    finalizando prototipo para exibir informações do setor

--- src/pages/setores.php
+++ src/pages/setores.php
@@ -1,5 +1,46 @@
 <?php include('partials/header-pages.php'); ?>
 
-<h1>setores</h1> 
+<div class="container mt-5 d-flex justify-content-left">
+    <div class="card" style="width: 18rem; margin-right: 20px;">
+        <div class="card-body">
+            <h3 class="card-title">Setor teste 1</h3>
+            <h4>cargos:</h4>
+            <p class="card-text">cargo teste 1</p>
+            <p class="card-text">cargo teste 2</p>
+            <h4>trabalhadores:</h4>
+            <p class="card-text">trabalhador teste 1</p>
+            <p class="card-text">trabalhador teste 2</p>
+            <h4>turnos:</h4>
+            <p class="card-text">matutino:</p>
+            <p class="card-text">1</p>
+            <p class="card-text">vespertino:</p>
+            <p class="card-text">1</p>
+            <p class="card-text">noturno:</p>
+            <p class="card-text">1</p>
+            <a href="#" class="btn btn-primary">Ver mais detalhes</a>
+        </div>
+    </div>
+
+
+    <div class="card" style="width: 18rem; margin-right: 20px;">
+        <div class="card-body">
+            <h3 class="card-title">Setor teste 2</h3>
+            <h4>cargos:</h4>
+            <p class="card-text">cargo teste 1</p>
+            <p class="card-text">cargo teste 2</p>
+            <h4>trabalhadores:</h4>
+            <p class="card-text">trabalhador teste 1</p>
+            <p class="card-text">trabalhador teste 2</p>
+            <h4>turnos:</h4>
+            <p class="card-text">matutino:</p>
+            <p class="card-text">1</p>
+            <p class="card-text">vespertino:</p>
+            <p class="card-text">1</p>
+            <p class="card-text">noturno:</p>
+            <p class="card-text">1</p>
+            <a href="#" class="btn btn-primary">Ver mais detalhes</a>
+        </div>
+    </div>
+</div>
 
 <?php include('partials/footer.php'); ?>
