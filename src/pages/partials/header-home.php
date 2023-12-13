8a242c880d3813da2aaabba17d5021da3870e4fa
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-13 17:22:47 -0300

    finalizando navbar

--- src/pages/partials/header-home.php
+++ src/pages/partials/header-home.php
@@ -10,13 +10,45 @@
 
     <body>
 
-    <div>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="src/pages/cadastroTrabalhador.php">cadastrar trabalhador</a></li>
-            <li><a href="src/pages/cadastroCargo.php">cadastrar cargo</a></li>
-            <li><a href="src/pages/cadastroSetor.php">cadastrar setor</a></li>
-        </ul>
-    </div>
+    <header class="bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>GRcomércios</h1>
+                </div>
+                <div class="col-md-6 text-right">
+                    <nav class="navbar navbar-expand-md">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="#">home</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="src/pages/setores.php">setores</a>
+                                </li>
+                                
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Cadastrar
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="src/pages/cadastroTrabalhador.php">cadastrar trabalhador</a>
+                                        <a class="dropdown-item" href="src/pages/cadastroCargo.php">cadastrar cargo</a>
+                                        <a class="dropdown-item" href="src/pages/cadastroSetor.php">cadastrar setor</a>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <h1>Página home</h1>
--- src/pages/partials/header-pages.php
+++ src/pages/partials/header-pages.php
@@ -10,11 +10,52 @@
 
     <body>
 
-    <div>
+    <!-- <div>
         <ul>
             <li><a href="../../index.php">Home</a></li>
             <li><a href="../pages/cadastroTrabalhador.php">cadastrar trabalhador</a></li>
             <li><a href="../pages/cadastroCargo.php">cadastrar cargo</a></li>
             <li><a href="../pages/cadastroSetor.php">cadastrar setor</a></li>
         </ul>
-    </div>
+    </div> -->
+
+    <header class="bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>GRcomércios</h1>
+                </div>
+                <div class="col-md-6 text-right">
+                    <nav class="navbar navbar-expand-md">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="../../index.php">home</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="../pages/setores.php">setores</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Cadastrar
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="../pages/cadastroTrabalhador.php">cadastrar trabalhador</a>
+                                        <a class="dropdown-item" href="../pages/cadastroCargo.php">cadastrar cargo</a>
+                                        <a class="dropdown-item" href="../pages/cadastroSetor.php">cadastrar setor</a>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
