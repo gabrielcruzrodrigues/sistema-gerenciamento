16faa1126f0a96b9e2c501247f593b06c717c01c
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-14 01:56:21 -0300

    criando sistema para aceitar apenas 3 turnos por cargo

--- src/controller/turnoController.php
+++ src/controller/turnoController.php
@@ -2,19 +2,28 @@
     include("../db/conexao.php"); 
     switch ($_REQUEST["action"]) {
         case 'create':
+
             $name = $_POST["name"];
             $cargo = $_POST["cargo"];
 
-            $sql = "INSERT INTO turno (name, cargo_id) VALUES ('{$name}', '{$cargo}')";
-            $res = $conn->query($sql);
+            $sql_verify = "SELECT * FROM turno WHERE cargo_id=".$cargo;
+            $res_verify = $conn->query($sql_verify);
+            $rows = $res_verify->num_rows;
 
-            if ($res == true) {
-                print "<script>alert('turno criado com suceso');</script>";
-                print "<script>location.href='../pages/setores.php';</script>";
+            if ($rows < 3) {
+                $sql = "INSERT INTO turno (name, cargo_id) VALUES ('{$name}', '{$cargo}')";
+                $res = $conn->query($sql);
+    
+                if ($res == true) {
+                    print "<script>alert('turno criado com suceso');</script>";
+                    print "<script>location.href='../pages/setores.php';</script>";
+                } else {
+                    print "<script>alert('Erro ao tentar criar novo turno');</script>";
+                    print "<script>location.href='../pages/cadastroSetor.php';</script>";
+                }
             } else {
-                print "<script>alert('Erro ao tentar criar novo turno');</script>";
-                print "<script>location.href='../pages/cadastroSetor.php';</script>";
+                print "<script>alert('Este cargo já possui os 3 cargos possíveis');</script>";
+                    print "<script>location.href='../pages/setores.php';</script>";
             }
-
             break;
 }
