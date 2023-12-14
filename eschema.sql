f3e5a6653c2b906ddb5aca17ff406d4e04a6c0ed
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-14 01:16:05 -0300

    criando base do cadastro do funcionário

--- eschema.sql
+++ eschema.sql
@@ -13,3 +13,24 @@ CREATE TABLE `cargo` (
     PRIMARY KEY (`id`),
     FOREIGN KEY (`setor_id`) REFERENCES `setor` (`id`)
 ) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `turno` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NOT NULL,
+    `cargo_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`cargo_id`) REFERENCES `cargo` (`id`)
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `trabalhador` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NOT NULL,
+    `turno_id`INT(11) NOT NULL,
+    `cargo_id` INT(11) NOT NULL,
+    `setor_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_cargo_id` (`cargo_id`),
+    FOREIGN KEY (`turno_id`) REFERENCES `turno` (`id`),
+    FOREIGN KEY (`cargo_id`) REFERENCES `cargo` (`id`),
+    FOREIGN KEY (`setor_id`) REFERENCES `setor` (`id`),
+) DEFAULT CHARSET=utf8mb4
--- src/pages/cadastroTrabalhador.php
+++ src/pages/cadastroTrabalhador.php
@@ -1,32 +1,60 @@
-<?php include('partials/header-pages.php'); ?> 
+<?php
+    include('partials/header-pages.php'); 
+    include("../db/conexao.php");
+
+    $sql_cargos = "SELECT * FROM cargo";
+    $sql_turnos = "SELECT * FROM turno";
+    $sql_setores = "SELECT * FROM setor";
+    $res_cargos = $conn->query($sql_cargos);
+    $res_turnos = $conn->query($sql_turnos);
+    $res_setores = $conn->query($sql_setores);
+ ?> 
 
 <div class="container mt-5">
     <h2>Cadastrar trabalhador</h2>
     <form>
         <div class="form-group">
             <label for="nome">Nome:</label>
-            <input type="text" class="form-control" id="nome" placeholder="Digite o nome do trabalhador">
+            <input type="text" class="form-control" id="nome" name="name" placeholder="Digite o nome do trabalhador">
         </div>
         <div class="form-group">
-            <label for="motivoContato">Cargo:</label>
-            <select class="form-control" id="motivoContato">
-                <option value="" disabled selected>Selecione um cargo</option>
-                <option value="duvida">teste</option>
-            </select>
+            <label for="setor">Cargo:</label>
+            <?php 
+
+            print "<select class='form-control' name='cargo'>";
+            print "<option value='' disabled selected>Selecione um cargo</option>";
+            while($row_cargos = $res_cargos->fetch_object()) {
+                print "<option value=".$row_cargos->id.">".$row_cargos->name."</option>";
+            }
+            print "</select>";
+
+            ?>
         </div>
         <div class="form-group">
-            <label for="motivoContato">Turno:</label>
-            <select class="form-control" id="motivoContato">
-                <option value="" disabled selected>Selecione um turno</option>
-                <option value="duvida">teste</option>
-            </select>
+            <label for="setor">Turno:</label>
+            <?php 
+
+            print "<select class='form-control' name='turno'>";
+            print "<option value='' disabled selected>Selecione um turno</option>";
+            while($row_turnos = $res_turnos->fetch_object()) {
+                print "<option value=".$row_turnos->id.">".$row_turnos->name."</option>";
+            }
+            print "</select>";
+
+            ?>
         </div>
         <div class="form-group">
-            <label for="motivoContato">Setor:</label>
-            <select class="form-control" id="motivoContato">
-                <option value="" disabled selected>Selecione um setor</option>
-                <option value="duvida">teste</option>
-            </select>
+            <label for="setor">setor:</label>
+            <?php 
+
+            print "<select class='form-control' name='setor'>";
+            print "<option value='' disabled selected>Selecione um setor</option>";
+            while($row_setor = $res_setores->fetch_object()) {
+                print "<option value=".$row_setor->id.">".$row_setor->name."</option>";
+            }
+            print "</select>";
+
+            ?>
         </div>
     
         <button type="submit" class="btn btn-primary">Cadastrar novo trabalhador</button>
